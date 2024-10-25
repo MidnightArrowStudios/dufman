2c62be3b508ba6f20d03f0e61519a564e53231f1
MidnightArrow <midnight.arrow.studio@gmail.com>
2024-10-24 21:22:28 -0400

    add additional copyright notices

---
+++ structs/__init__.py
@@ -0,0 +1,5 @@
+# ============================================================================ #
+# Copyright (c) 2024, Midnight Arrow.
+# https://github.com/MidnightArrowStudios/dufman
+# Licensed under the MIT license.
+# ============================================================================ #
