0a0441143cec37ae6ed931c0c7030410b94da05b
MidnightArrow <midnight.arrow.studio@gmail.com>
2024-10-28 16:57:23 -0400

    add import statements

--- __init__.py
+++ __init__.py
@@ -3,3 +3,15 @@
 # https://github.com/MidnightArrowStudios/dufman
 # Licensed under the MIT license.
 # ============================================================================ #
+
+from . import create
+from . import datatypes
+from . import enums
+from . import exceptions
+from . import file
+from . import library
+from . import observers
+from . import scene
+from . import structs
+from . import url
+from . import utilities
--- structs/__init__.py
+++ structs/__init__.py
@@ -3,3 +3,8 @@
 # https://github.com/MidnightArrowStudios/dufman
 # Licensed under the MIT license.
 # ============================================================================ #
+
+from . import geometry
+from . import modifier
+from . import node
+from . import uv_set
