fac6c8773574d4a0fe6de8a1d8393c7fafa48b2b
MidnightArrow <midnight.arrow.studio@gmail.com>
2024-12-14 02:30:24 -0500

    removed outdated import statement

--- dufman/url.py
+++ dufman/url.py
@@ -8,7 +8,7 @@
 from __future__ import annotations
 from dataclasses import dataclass
 from pathlib import Path
-from typing import Any, Self
+from typing import Any
 from urllib.parse import ParseResult, quote, unquote, urlparse, urlunparse
 
 from .exceptions import IncorrectArgument
