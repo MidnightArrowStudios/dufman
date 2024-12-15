f108e2e2eed076836e4daa527a5a0c49373453f0
MidnightArrow <midnight.arrow.studio@gmail.com>
2024-12-14 21:46:39 -0500

    revised unittest

--- tests/test_address.py
+++ tests/test_address.py
@@ -29,9 +29,6 @@ class TestAddress(TestCase):
     def test_filepath(self:TestAddress) -> None:
 
         canonical_url:str = "/data/DAZ%203D/Genesis%208/Female/Genesis8Female.dsf"
-        foreslash_url:str = "/data/DAZ 3D/Genesis 8/Female/Genesis8Female.dsf"
-        backslash_url:str = r"\data\DAZ 3D\Genesis 8\Female\Genesis8Female.dsf"
-        double_backslash_url:str = "\\data\\DAZ 3D\\Genesis 8\\Female\\Genesis8Female.dsf"
 
         def check_address(url_string:Any, *, node_name:str = None, asset_id:str = None, property_path:str = None) -> None:
             nonlocal canonical_url
@@ -44,53 +41,25 @@ class TestAddress(TestCase):
             self.assertEqual(address.property_path, property_path)
             return
 
-        # Filepath string
-        check_address(foreslash_url)
-        check_address(backslash_url)
-        check_address(double_backslash_url)
+        def check_filepath(url_string:str) -> None:
+            asset_id:str = "Genesis8Female"
+            node_name:str = "hip"
+            props:str = "translation/y"
 
-        # Filepath as Path object
-        check_address(Path(foreslash_url))
-        check_address(Path(backslash_url))
-        check_address(Path(double_backslash_url))
+            check_address(f"{url_string}")
+            check_address(f"{url_string}#{asset_id}", asset_id=asset_id)
+            check_address(f"{node_name}:{url_string}#{node_name}", node_name=node_name, asset_id=node_name)
+            check_address(f"{node_name}:{url_string}#{node_name}?{props}", node_name=node_name, asset_id=node_name, property_path=props)
 
-        # Test Asset ID
-        asset_id:str = "Genesis8Female"
+            check_address(Path(f"{url_string}"))
+            check_address(Path(f"{url_string}#{asset_id}"), asset_id=asset_id)
+            check_address(Path(f"{node_name}:{url_string}#{node_name}"), node_name=node_name, asset_id=node_name)
+            check_address(Path(f"{node_name}:{url_string}#{node_name}?{props}"), node_name=node_name, asset_id=node_name, property_path=props)
 
-        # String
-        check_address(f"{foreslash_url}#{asset_id}", asset_id=asset_id)
-        check_address(f"{backslash_url}#{asset_id}", asset_id=asset_id)
-        check_address(f"{double_backslash_url}#{asset_id}", asset_id=asset_id)
+            return
 
-        # Path object
-        check_address(Path(f"{foreslash_url}#{asset_id}"), asset_id=asset_id)
-        check_address(Path(f"{backslash_url}#{asset_id}"), asset_id=asset_id)
-        check_address(Path(f"{double_backslash_url}#{asset_id}"), asset_id=asset_id)
-
-        # Test node name
-        node_name:str = "hip"
-
-        # String
-        check_address(f"{node_name}:{foreslash_url}#{node_name}", node_name=node_name, asset_id=node_name)
-        check_address(f"{node_name}:{backslash_url}#{node_name}", node_name=node_name, asset_id=node_name)
-        check_address(f"{node_name}:{double_backslash_url}#{node_name}", node_name=node_name, asset_id=node_name)
-
-        # Path object
-        check_address(Path(f"{node_name}:{foreslash_url}#{node_name}"), node_name=node_name, asset_id=node_name)
-        check_address(Path(f"{node_name}:{backslash_url}#{node_name}"), node_name=node_name, asset_id=node_name)
-        check_address(Path(f"{node_name}:{double_backslash_url}#{node_name}"), node_name=node_name, asset_id=node_name)
-
-        # Test property path
-        props:str = "translation/y"
-
-        # String
-        check_address(f"{node_name}:{foreslash_url}#{node_name}?{props}", node_name=node_name, asset_id=node_name, property_path=props)
-        check_address(f"{node_name}:{backslash_url}#{node_name}?{props}", node_name=node_name, asset_id=node_name, property_path=props)
-        check_address(f"{node_name}:{double_backslash_url}#{node_name}?{props}", node_name=node_name, asset_id=node_name, property_path=props)
-
-        # Path object
-        check_address(Path(f"{node_name}:{foreslash_url}#{node_name}?{props}"), node_name=node_name, asset_id=node_name, property_path=props)
-        check_address(Path(f"{node_name}:{backslash_url}#{node_name}?{props}"), node_name=node_name, asset_id=node_name, property_path=props)
-        check_address(Path(f"{node_name}:{double_backslash_url}#{node_name}?{props}"), node_name=node_name, asset_id=node_name, property_path=props)
+        check_filepath("/data/DAZ 3D/Genesis 8/Female/Genesis8Female.dsf")
+        check_filepath(r"\data\DAZ 3D\Genesis 8\Female\Genesis8Female.dsf")
+        check_filepath("\\data\\DAZ 3D\\Genesis 8\\Female\\Genesis8Female.dsf")
 
         return
