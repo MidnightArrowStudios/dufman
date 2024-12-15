635441bee592d98f3038cb07b3fb743480a9e220
MidnightArrow <midnight.arrow.studio@gmail.com>
2024-12-14 23:12:49 -0500

    adjusted function to use AssetAddress object

--- dufman/structs/node.py
+++ dufman/structs/node.py
@@ -18,7 +18,7 @@ from ..library import (
     get_single_property_from_library,
 )
 
-from ..url import AssetURL, parse_url_string
+from ..url import AssetAddress
 
 from .channel import DsonChannelFloat, DsonChannelVector
 from .formula import DsonFormula
@@ -230,7 +230,7 @@ def _inherits_scale_default(struct:DsonNode) -> bool:
         return True
 
     # Create variables to access property.
-    address:AssetURL = parse_url_string(struct.parent)
+    address:AssetAddress = AssetAddress.create_from_url(struct.parent)
     property_path:list[Any] = [ "node_library", address.asset_id, "type" ]
 
     # Get type as string
