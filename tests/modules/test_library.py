8f311e76cd2f2c61c8842e32c26beccb955fb807
MidnightArrow <midnight.arrow.studio@gmail.com>
2024-12-26 21:46:15 -0500

    added unit test method, de-inherited object

--- tests/modules/test_library.py
+++ tests/modules/test_library.py
@@ -1,28 +1,62 @@
 from __future__ import annotations
+from pathlib import Path
+from unittest import TestCase
 
+from dufman import file
 from dufman.enums import LibraryType
-
 from dufman.library import (
+    find_library_containing_asset_id,
     get_all_asset_urls_from_library,
     get_asset_json_from_library,
     get_node_hierarchy_urls_from_library,
     get_single_property_from_library,
 )
 
-from tests.abstract.test_directory import TestDirectory
 
-
-class TestLibrary(TestDirectory):
+class TestLibraryModule(TestCase):
     """Unit testing for DUFMan's library querying functions."""
+
+    def setUp(self:TestLibraryModule) -> None:
+        self.content_directory = Path("F:/Daz3D")
+        file.add_content_directory(self.content_directory)
+        return
+
+    def tearDown(self:TestLibraryModule) -> None:
+        file.remove_content_directory(self.content_directory)
+        return
+
 
     # ======================================================================== #
     #                                                                          #
     # ======================================================================== #
 
-    def test_get_all_asset_urls(self:TestLibrary) -> None:
+    def test_find_library(self:TestLibraryModule) -> None:
+
+        url_string:str = "/data/DAZ 3D/Genesis 8/Female/Genesis8Female.dsf"
+
+        # Geometry
+        geo_url:str = f"{url_string}#geometry"
+        self.assertEqual(find_library_containing_asset_id(geo_url), LibraryType.GEOMETRY)
+
+        # Node
+        hip_url:str = f"{url_string}#hip"
+        self.assertEqual(find_library_containing_asset_id(hip_url), LibraryType.NODE)
+
+        # Modifier
+        mod_url:str = f"{url_string}#SkinBinding"
+        self.assertEqual(find_library_containing_asset_id(mod_url), LibraryType.MODIFIER)
+
+        return
+
+
+    # ======================================================================== #
+    #                                                                          #
+    # ======================================================================== #
+
+    def test_get_all_asset_urls(self:TestLibraryModule) -> None:
 
         url_string:str = "/data/DAZ%203D/Genesis%208/Female/Genesis8Female.dsf"
-        urls:list[str] = get_all_asset_urls_from_library(url_string, "node_library")
+        urls:list[str] = get_all_asset_urls_from_library(url_string, LibraryType.NODE)
 
         self.assertIsNotNone(urls)
         self.assertEqual(len(urls), 171)
@@ -38,7 +72,7 @@ class TestLibrary(TestDirectory):
     #                                                                          #
     # ======================================================================== #
 
-    def test_get_asset_json(self:TestLibrary) -> None:
+    def test_get_asset_json(self:TestLibraryModule) -> None:
 
         url_string:str = "/data/DAZ 3D/Genesis 8/Female/Genesis8Female.dsf"
         asset_id:str = "Genesis8Female"
@@ -68,7 +102,7 @@ class TestLibrary(TestDirectory):
     #                                                                          #
     # ======================================================================== #
 
-    def test_get_node_hierarchy_urls(self:TestLibrary) -> None:
+    def test_get_node_hierarchy_urls(self:TestLibraryModule) -> None:
 
         url_string:str = "/data/DAZ%203D/Genesis%208/Female/Genesis8Female.dsf"
         asset_id:str = "Genesis8Female"
@@ -88,7 +122,12 @@ class TestLibrary(TestDirectory):
     #                                                                          #
     # ======================================================================== #
 
-    def test_general_scale_property(self:TestLibrary) -> None:
+    def test_single_property_general_scale(self:TestLibraryModule) -> None:
+        """Unit test for the \"get_single_property_from_library()\" method.
+        
+        Daz Studio has a 'quirk' where 'scale/general' is converted under the
+        hood to 'general_scale', making this worthy of a dedicated unit test.
+        """
 
         url_string:str = "/data/DAZ%203D/Genesis%208/Female/Genesis8Female.dsf"
         property_path:list[str] = [ "node_library", "lThighTwist", "scale", "general" ]
